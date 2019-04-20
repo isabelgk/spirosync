a1a9424855dc0f3df510bc0bd02b76a2269d31e5
Shannon Duffy <shannonduffy954@gmail.com>
2019-04-20 06:31:11 -0400

    added movement to OnBeatBubble

--- graphics.py
+++ graphics.py
@@ -11,10 +11,12 @@ import numpy as np
 
 class Character(object):
     """Character draws the character, handles audio data, and draws bubbles behind the character"""
-    def __init__(self, spotify_player):
+    def __init__(self, spotify_player = None):
         super().__init__()
 
         self.audio = spotify_player
+
+        one_bubble = OnBeatBubble((Window.width*3/4,Window.height/2), 50, (1,1,0))
 
         # Time keeping
         self.duration = duration
@@ -42,36 +44,36 @@ class Character(object):
         self.time += dt
 
 class OnBeatBubble(InstructionGroup):
-    def __init__(self, pos=(0, 0), start_size=20, duration=1):
+    def __init__(self, pos, radius, color, speed):
         super().__init__()
 
+        self.original_pos = pos
+
         # Color
-        self.color = Color(hsv=(pos[0]/Window.width, 1, 1))
-        self.color.a = 0.6
+        self.color = Color(*color)
         self.add(self.color)
 
         # Shape
-        self.dot = CEllipse(cpos=pos, segments=20)
-        self.dot.csize = (start_size, start_size)
+        self.dot = CEllipse(cpos=pos, csize = (2*radius, 2*radius), sizesegments=20)
         self.add(self.dot)
 
         # Animation
         self.radius_anim = KFAnim((0, start_size), (duration, 0))
-        self.pos_anim = KFAnim((0, pos[0], pos[1]), (duration*3, pos[0], 0))
+        self.pos_anim = KFAnim((0, pos[0]), (pos[0]*speed, 0))
 
         # Time keeping
         self.time = 0
         self.on_update(0)
 
     def on_update(self, dt):
-        rad = self.radius_anim.eval(self.time)
-        self.dot.csize = 2*rad, 2*rad
+        # rad = self.radius_anim.eval(self.time)
+        # self.dot.csize = 2*rad, 2*rad
 
         pos = self.pos_anim.eval(self.time)
-        self.dot.cpos = pos
+        self.dot.cpos = (pos, self.original_pos[1])
 
         self.time += dt
-        return self.radius_anim.is_active(self.time)
+        # return self.radius_anim.is_active(self.time)
 
 class OffBeatSpray(InstructionGroup):
     def __init__(self, pos=(0, 0), start_size=20, duration=1):
